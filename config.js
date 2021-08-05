9b72e6c87b3915f19bea06aacbad84f90dcbf445
Jayh2510 <88484345+Jayh2510@users.noreply.github.com>
2021-08-05 16:39:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,17 +3,17 @@ const CONFIG = {
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Đằng sau chữ yêu đây là anh`,
     btnIntro: 'Ahihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng em đã iu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy anh đẹp zaiii ',
     btnYes: 'Anh kì quá à :">',
     btnNo: 'Méo :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Do anh đẹp trai vcl',
+    reply: 'Do anh đẹp trai vclllll :>>>>',
     mess: 'Anh biết mà 🥰. Yêu em nhìuuuuu 😘😘',
     messDesc: 'Tối nay 7h anh qua đi nhậu nhé :3.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/messages/t/100003770284669'
+    messLink: 'm.me/nhanthanh.nguyen.58'
 }
